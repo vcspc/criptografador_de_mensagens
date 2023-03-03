4aa80676d827e870e14f448f51a0fa201999f111
vinicius costa <vitonilcius@gmail.com>
2023-03-03 17:17:02 -0300

    criação do script

---
+++ script.js
@@ -0,0 +1,15 @@
+class Message {
+
+    constructor(message, result, key){
+        this.message = message
+        this.result = result
+        this.key = key
+    }
+
+    get message (){
+        return this.message
+    }
+
+}
+
+
