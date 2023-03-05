f865567a4e755d8ece1f4735ca64d8f4bb9fe876
vinicius costa <vitonilcius@gmail.com>
2023-03-05 18:40:09 -0300

    Inclusão da lógica da chave

--- script.js
+++ script.js
@@ -1,20 +1,103 @@
-let text = document.getElementById("input-message");
+function start(){
 
-function cryptography (){
-    let message = text.value;
-    let result = 5;
-    let key = 145;
-
-    return {
-       /* increment(){
-            message++;
-        },*/
-        getMessage(){
-            return message;
-        }
-    }
+    let buttonCode = document.querySelector('#button-code');
+    let buttonDecode = document.querySelector('#button-decode');
+    let buttonCopy = document.querySelector('#button-copy');
+        buttonCode.addEventListener('click', handButtonClickCode);
+        buttonDecode.addEventListener('click', handButtonClickDecode);
+        buttonCopy.addEventListener('click', handButtonClickCopy);
+        
 }
 
-const message1 = cryptography();
+    function handButtonClickCode(){
+        let textMessage = document.querySelector('#input-message');
+        let codeResult = document.querySelector('#result');
 
-console.log(message1.getMessage());
+
+        let message = textMessage.value;
+        let messageCode = [];
+        let keyMessageCode = [];
+        let result = "";
+        let messageTranslated = [];
+    
+
+        for(let cont = 0; cont < message.length; cont++){
+            messageCode.push(message.charCodeAt(cont))
+            keyMessageCode.push(addMessageCode(messageCode[cont], message))
+            messageTranslated.push(String.fromCharCode(keyMessageCode[cont]))
+            result = result + messageTranslated[cont]
+        }
+
+        codeResult.value = result;
+        
+    }
+
+    function handButtonClickDecode(){
+        let textMessage = document.querySelector('#input-message');
+        let codeResult = document.querySelector('#result');
+
+
+        let message = textMessage.value;
+        let messageCode = [];
+        let keyMessageCode = [];
+        let result = "";
+        let messageTranslated = [];
+    
+
+        for(let cont = 0; cont < message.length; cont++){
+            messageCode.push(message.charCodeAt(cont))
+            keyMessageCode.push(removeMessageCode(messageCode[cont], message))
+            messageTranslated.push(String.fromCharCode(keyMessageCode[cont]))
+            result = result + messageTranslated[cont]
+        }
+
+        codeResult.value = result;
+    }
+
+    function addMessageCode(number, message){
+
+       for(let cont = 0; cont < message.length; cont++){
+
+            let value = number + key[cont];
+
+
+            return value;
+        }
+    }
+        
+    function removeMessageCode(number, message){
+         
+        for(let cont = 0; cont < message.length; cont++){
+    
+                let value = number - key[cont];
+    
+                return value;
+            }
+        }
+    
+    function handButtonClickCopy(){
+
+        let codeResult = document.querySelector('#result');
+        
+        navigator.clipboard.writeText(codeResult.value);
+
+        var copied = codeResult.value;
+
+        alert('"' + copied + '"' + ' copiado');
+    }
+
+    let key = [];
+
+    function captureKey (){
+
+    let key1 = document.querySelector('#key');
+    let key2 = key1.value
+    
+    for(let cont; cont < key2.length; cont++){
+    key.push(key[cont])
+    }
+
+    }
+    
+
+start();
